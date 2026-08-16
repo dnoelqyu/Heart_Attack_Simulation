27097dbc76d41f6fdb4b5d4bc3b56f5bdbb18353
dnoelqyu <diftires@gmail.com>
2026-08-16 11:49:53 +0700

    Add files via upload

---
+++ HeartAttack.bat
@@ -0,0 +1,8 @@
+@echo off
+title HEART ATTACK SIMULATOR
+echo Menjalankan Aplikasi...
+echo --------------------------------------
+echo JANGAN TUTUP JENDELA INI
+echo --------------------------------------
+py -m streamlit run heart_attack_dashboard_id.py
+pause
