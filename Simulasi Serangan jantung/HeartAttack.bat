a4d597e46ac860dd6b12d2bf6c68a991a00c2958
dnoelqyu <diftires@gmail.com>
2026-08-16 11:57:12 +0700

    Delete Simulasi Serangan jantung directory

--- Simulasi Serangan jantung/HeartAttack.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title HEART ATTACK SIMULATOR
-echo Menjalankan Aplikasi...
-echo --------------------------------------
-echo JANGAN TUTUP JENDELA INI
-echo --------------------------------------
-py -m streamlit run heart_attack_dashboard_id.py
-pause
